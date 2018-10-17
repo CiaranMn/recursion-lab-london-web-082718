1113de4b18b7166e8037ca443d9500b46b3fb67a
Ciaran M <cmorinan@gmail.com>
2018-10-17 13:52:51 +0100

    working

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// Code your solution here!
+function printString(string) {
+  if (string.length <= 0) return;
+  console.log(string[0]);
+  printString(string.slice(1,string.length));
+}
+
+function reverseString(string) {
+  if (string.length === 1) {return string}
+  return reverseString(string.slice(1)) + string[0]
+}
 
